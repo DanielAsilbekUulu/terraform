d4034ca49484b61bfee8535e90c8a47955852ee1
DanielAsilbekUulu <asilbekuuludaniel2003@gmail.com>
2024-12-11 18:22:46 -0800

    few fi

--- terraform-old/main.tf
+++ terraform-old/main.tf
@@ -1,14 +1,14 @@
 # use data source to pull the AMI id 
-data "aws_ami" "forgotAIMbek" {
+data "aws_ami" "with_apacheAMI" {
   owners = ["self"]
   filter {
     name   = "name"
-    values = ["*forgotAIM*"]
+    values = ["*apache*"]
   }
 }
 # create a EC2 instance
 resource "aws_instance" "hisec2" {
-  ami = data.aws_ami.forgotAIMbek.id
+  ami = data.aws_ami.with_apacheAMI.id
  # ami           = var.ami      (it is for the ami for different environment)
   instance_type               = var.my_instance_type             #instance_type isn't variable,it must be there. my_instance_type is variable, we can change it however we want!
   associate_public_ip_address = true
